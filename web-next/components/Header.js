119d0c42fc027c16f1f881ff6b04faf03d20a57c
muhammadmada <muhammad.mada.ibrahim@gmail.com>
2023-03-30 16:02:41 +0700

    update branch

---
+++ web-next/components/Header.js
@@ -0,0 +1,10 @@
+import headerStyles from '../styles/Header.module.css';
+const Header = () => {
+    return (
+        <div>
+           <h1><span>Webdev</span></h1> 
+        </div>
+    )
+}
+
+export default Header;
--- web-next/pages/index.tsx
+++ web-next/pages/index.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Toko Obat Tok Abah</title>
+      </Head>
       <h1 className={inter.className}>
         Hello World
       </h1>
